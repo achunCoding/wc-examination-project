9cd9796531b24ec3d8e4334421131a1515f867e9
wyc <wycfight@163.com>
2022-06-10 17:34:56 +0800

    feat: 初始化项目

---
+++ README.md
@@ -0,0 +1,13 @@
+# wc-examination-project
+
+## 前言
+
+`wc-examination-project`项目致力于打造一个完整的考试系统Demo，采用现阶段流行技术实现。
+
+## 项目文档
+
+- 文档地址：[https://www.yuque.com/cnzwwyy/mz2xgc/iyqf98](https://www.yuque.com/cnzwwyy/mz2xgc/iyqf98)
+
+## 项目介绍
+
+`wc-examination-project`项目是考试系统，包括前台考试系统及后台管理系统，基于SpringBoot+MyBatis实现。
---
+++ src/main/java/cn/study/exam/WcExaminationProjectApplication.java
@@ -0,0 +1,13 @@
+package cn.study.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcExaminationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcExaminationProjectApplication.class, args);
+    }
+
+}
